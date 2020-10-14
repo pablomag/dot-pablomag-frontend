fd7b40d93e8b9444d45b112359443de1b09db1b7
pablomag <pablomag@gmail.com>
2020-10-14 23:06:10 +0300

    Dotenv configured

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,4 @@
 export const APP_TITLE = ".pablomag";
 
 export const API_URL = process.env.API_URL || "http://localhost:5555/api";
-export const IMG_SERVICE_URL =
-    process.env.IMG_SERVICE_URL || "http://localhost:3333";
+export const IMG_SERVICE_URL = process.env.IMG_SERVICE_URL || "http://localhost:3333";
