f0e3e68125686b125ca033f9a3e514a41f7beebc
pablomag <pablomag@gmail.com>
2020-10-27 12:32:24 +0300

    Fixed pm2 commands permissions

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/*", (_, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(APP_PORT);
