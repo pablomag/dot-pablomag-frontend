96169c071d600c0b63ee145cefcf6cecf4960849
pablomag <pablomag@gmail.com>
2020-10-29 17:35:20 +0300

    Fixed env vars

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const APP_TITLE = ".pablomag";
 
-export const API_URL = process.env.API_URL || "http://localhost:5555/api";
-export const IMG_SERVICE_URL = process.env.IMG_SERVICE_URL || "http://localhost:3333";
+export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5555/api";
+export const IMG_SERVICE_URL = process.env.REACT_APP_IMG_SERVICE_URL || "http://localhost:3333";
